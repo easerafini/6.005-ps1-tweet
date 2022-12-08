ddb9570d2f7f8a824edae20aea8c304a7c50fb65
Eduardo Serafini <easerafini@gmail.com>
2022-12-07 23:09:03 -0300

    Actualizo Filter

--- src/twitter/Filter.java
+++ src/twitter/Filter.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Filter consists of methods that filter a list of tweets for those matching a
@@ -18,68 +21,72 @@ import java.util.List;
  */
 public class Filter {
 
-    /**
-     * Find tweets written by a particular user.
-     * 
-     * @param tweets   a list of tweets with distinct ids, not modified by this
-     *                 method.
-     * @param username Twitter username, required to be a valid Twitter username as
-     *                 defined by Tweet.getAuthor()'s spec.
-     * @return all and only the tweets in the list whose author is username, in the
-     *         same order as in the input list.
-     */
-    public static List<Tweet> writtenBy(List<Tweet> tweets, String username) {
-        return tweets.stream().filter((e) -> e.getAuthor().equals(username)).toList();
-    }
+	/**
+	 * Find tweets written by a particular user.
+	 * 
+	 * @param tweets   a list of tweets with distinct ids, not modified by this
+	 *                 method.
+	 * @param username Twitter username, required to be a valid Twitter username as
+	 *                 defined by Tweet.getAuthor()'s spec.
+	 * @return all and only the tweets in the list whose author is username, in the
+	 *         same order as in the input list.
+	 */
+	public static List<Tweet> writtenBy(List<Tweet> tweets, String username) {
+		return tweets.stream().filter((e) -> e.getAuthor().equals(username)).toList();
+	}
 
-    /**
-     * Find tweets that were sent during a particular timespan.
-     * 
-     * @param tweets   a list of tweets with distinct ids, not modified by this
-     *                 method.
-     * @param timespan timespan
-     * @return all and only the tweets in the list that were sent during the
-     *         timespan, in the same order as in the input list.
-     */
-    public static List<Tweet> inTimespan(List<Tweet> tweets, Timespan timespan) {
-        return tweets.stream().filter(
-                (e) -> (e.getTimestamp().isAfter(timespan.getStart()) || e.getTimestamp().equals(timespan.getStart()))
-                        && (e.getTimestamp().isBefore(timespan.getEnd()) || e.getTimestamp().equals(timespan.getEnd())))
-                .toList();
-    }
+	/**
+	 * Find tweets that were sent during a particular timespan.
+	 * 
+	 * @param tweets   a list of tweets with distinct ids, not modified by this
+	 *                 method.
+	 * @param timespan timespan
+	 * @return all and only the tweets in the list that were sent during the
+	 *         timespan, in the same order as in the input list.
+	 */
+	public static List<Tweet> inTimespan(List<Tweet> tweets, Timespan timespan) {
+		return tweets.stream().filter(
+				(e) -> (e.getTimestamp().isAfter(timespan.getStart()) || e.getTimestamp().equals(timespan.getStart()))
+						&& (e.getTimestamp().isBefore(timespan.getEnd()) || e.getTimestamp().equals(timespan.getEnd())))
+				.toList();
+	}
 
-    /**
-     * Find tweets that contain certain words.
-     * 
-     * @param tweets a list of tweets with distinct ids, not modified by this
-     *               method.
-     * @param words  a list of words to search for in the tweets. A word is a
-     *               nonempty sequence of nonspace characters.
-     * @return all and only the tweets in the list such that the tweet text (when
-     *         represented as a sequence of nonempty words bounded by space
-     *         characters and the ends of the string) includes *at least one* of the
-     *         words found in the words list. Word comparison is not case-sensitive,
-     *         so "Obama" is the same as "obama". The returned tweets are in the
-     *         same order as in the input list.
-     */
-    public static List<Tweet> containing(List<Tweet> tweets, List<String> words) {
-        final List<Tweet> result = new ArrayList<>();
-        if (!tweets.isEmpty() && !words.isEmpty()) {
-            for (Tweet tweet : tweets) {
-                String text = tweet.getText().toLowerCase();
-                List<String> wordsText = Arrays.asList(text.split("[\\s\\W]"));
-                boolean match = false;
-                int index = 0;
-                while (!match && (index < words.size())) {
-                    if (wordsText.contains(words.get(index).toLowerCase())) {
-                        match = true;
-                        result.add(tweet);
-                    }
-                    ++index;
-                }
-            }
-        }
-        return Collections.unmodifiableList(result);
-    }
+	/**
+	 * Find tweets that contain certain words.
+	 * 
+	 * @param tweets a list of tweets with distinct ids, not modified by this
+	 *               method.
+	 * @param words  a list of words to search for in the tweets. A word is a
+	 *               nonempty sequence of nonspace characters.
+	 * @return all and only the tweets in the list such that the tweet text (when
+	 *         represented as a sequence of nonempty words bounded by space
+	 *         characters and the ends of the string) includes *at least one* of the
+	 *         words found in the words list. Word comparison is not case-sensitive,
+	 *         so "Obama" is the same as "obama". The returned tweets are in the
+	 *         same order as in the input list.
+	 */
+	public static List<Tweet> containing(List<Tweet> tweets, List<String> words) {
+		final List<Tweet> result = new ArrayList<>();
+		if (!tweets.isEmpty() && !words.isEmpty()) {
+			for (Tweet tweet : tweets) {
+				String tweetText = tweet.getText();
+				Pattern pattern = Pattern.compile("\\w+");
+				Matcher matcher = pattern.matcher(tweetText);
+				boolean match = false;
+				while(!match && matcher.find()) {
+					String wordTweetText = matcher.group();
+					int index = 0;
+					while (!match && (index < words.size())) {
+						if (wordTweetText.equalsIgnoreCase(words.get(index))) {
+							match = true;
+							result.add(tweet);
+						}
+						++index;
+					}
+				}
+			}
+		}
+		return Collections.unmodifiableList(result);
+	}
 
 }
--- test/twitter/SocialNetworkTest.java
+++ test/twitter/SocialNetworkTest.java
@@ -5,6 +5,7 @@ package twitter;
 
 import static org.junit.Assert.*;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,18 +21,60 @@ public class SocialNetworkTest {
      * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
      * Make sure you have partitions.
      */
-    
+    /*
+     * test strategy for guessFollowsGraph;
+     * tweets.size: 0, 1, >1;
+     * tweets.size >1: same autor, diferent autor;
+     * follow: 0, 1, >1;
+     */
+	
+	private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
+	private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
+	private static final Instant d3 = Instant.parse("2016-02-17T12:00:00Z");
+	private static final Instant d4 = Instant.parse("2016-02-17T13:00:00Z");
+	private static final Instant d5 = Instant.parse("2016-02-17T14:00:00Z");
+
+	private static final Tweet tweetCeroMencionCeroUsuarioInvalido = new Tweet(1, "alyssa",
+			"is it reasonable to talk about rivest so much?", d1);
+	private static final Tweet tweetUnaMencionCeroUsuarioInvalido = new Tweet(2, "bbitdiddle",
+			"@MikE rivest talk in 30 minutes #hype", d2);
+	private static final Tweet tweetMayorUnaMencionCeroUsuarioInvalido = new Tweet(3, "tom",
+			"@Dora rivest talk in 30 @Mike minutes #hype @victor", d3);
+	private static final Tweet tweetCeroMencionUnUsuarioInvalido = new Tweet(1, "alyssa",
+			"is it reasonable to talk @mike! about rivest so much?", d1);
+	private static final Tweet tweetUnaMencionUnUsuarioInvalido = new Tweet(2, "bbitdiddle",
+			"@MikE rivest talk mike@gmail.com in 30 minutes #hype", d2);
+	private static final Tweet tweetMayorUnaMencionUnUsuarioInvalido = new Tweet(3, "tom",
+			"@Dora rivest talk in 30 @Mike minutes @victor? #hype @victor", d3);
+	private static final Tweet tweetCeroMencionMayorUnUsuarioInvalido = new Tweet(1, "alyssa",
+			"is it @mike! mail@bob.com reasonable to talk about rivest so much?", d1);
+	private static final Tweet tweetUnaMencionMayorUnUsuarioInvalido = new Tweet(2, "bbitdiddle",
+			"@MikE rivest @mike! mail@bob.com talk in 30 minutes #hype", d2);
+	private static final Tweet tweetMayorUnaMencionMayorUnUsuarioInvalido = new Tweet(3, "tom",
+			"@Dora rivest talk in 30 @Mike minutes @mike! mail@bob.com #hype @victor", d3);
+	private static final Tweet tweetCaseInsensitive = new Tweet(4, "Mike",
+			"@Mike @mike rivest talk in 30 minutes #hype @miKe", d4);
+	private static final Tweet tweetCaseInsensitiveMayorUnTweet = new Tweet(4, "Mike",
+			"@Mike @mike @Victor @VictoR rivest talk @mike! mail@bob.com in 30 minutes #hype @miKe", d5);
+
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
+    /*
+     * guessFollowsGraph
+     */
+    /*
+     * tweets.size = 0
+     * follow: 0
+     */
     @Test
     public void testGuessFollowsGraphEmpty() {
         Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<>());
-        
         assertTrue("expected empty graph", followsGraph.isEmpty());
     }
+
     
     @Test
     public void testInfluencersEmpty() {
